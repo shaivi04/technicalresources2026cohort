73b3caa4c62ba009d3c20509be38d424f1921ed5
shaivi04 <110369719+shaivi04@users.noreply.github.com>
2023-01-16 00:30:44 +0530

    Submitting Q2 of assignment

---
+++ a2_q2.py
@@ -0,0 +1,11 @@
+marks=int(input("enter marks:"))                        #Q2
+if marks>95: 
+   print("Grade A+")
+elif marks>90:
+   print("Grade A")
+elif marks>85:
+   print("Grade B+")
+elif marks>80:
+   print("Grade B")
+else:
+   print("Grade C")
