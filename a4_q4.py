9fe10b1dbc067df8c7a6d737832f0081c611d9e2
shaivi04 <110369719+shaivi04@users.noreply.github.com>
2023-01-18 00:21:37 +0530

    submitting q4 of assignmet

---
+++ a4_q4.py
@@ -0,0 +1,16 @@
+num=int(input("Enter range:"))
+
+print("Prime numbers:",end=' ')
+
+for n in range(1,num):                                     #q4
+
+    for i in range(2,n):
+
+        if(n%i==0):
+
+            break
+
+    else:
+
+        print(n,end=' ')        
+     
